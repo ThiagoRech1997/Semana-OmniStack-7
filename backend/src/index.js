a6df9103808974916452bc3b36f5b9f157073b30
Thiago Fernando Rech <thiagorech.1997@gmail.com>
2025-07-01 16:21:12 -0300

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,8 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://semana:semana@cluster0-1ckxc.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/semanaomnistack7';
+mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use((req, res, next) =>{
     req.io = io;
@@ -18,4 +19,4 @@ app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes'));
 
-server.listen(3050);
+server.listen(3333);
